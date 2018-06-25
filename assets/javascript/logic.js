ff93023dfee2ed006608248ac4f40eb16f6860d1
Nick Iantosca <nickiantosca@Nicks-MacBook-Pro.local>
2018-06-25 17:56:03 -0400

    Receiving event data

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -2,19 +2,19 @@ $(document).ready(function () {
 
     //Firebase
     
-    var config = {
-        apiKey: "SyBhvDWxzgJSUD0VuEd4bszwgflEB5KpvSg",
-        authDomain: "test-820ea.firebaseapp.com",
-        databaseURL: "https://test-820ea.firebaseio.com",
-        projectId: "test-820ea",
-        storageBucket: "test-820ea.appspot.com",
-        messagingSenderId: "776526119"
-    };
-    firebase.initializeApp(config);
+    // var config = {
+    //     apiKey: "SyBhvDWxzgJSUD0VuEd4bszwgflEB5KpvSg",
+    //     authDomain: "test-820ea.firebaseapp.com",
+    //     databaseURL: "https://test-820ea.firebaseio.com",
+    //     projectId: "test-820ea",
+    //     storageBucket: "test-820ea.appspot.com",
+    //     messagingSenderId: "776526119"
+    // };
+    // firebase.initializeApp(config);
     
-    // Declare Variables
+    // // Declare Variables
     
-    var dataRef = firebase.database();
+    // var dataRef = firebase.database();
     var name = "";
     var location = "";
     var start = 0;
@@ -22,75 +22,79 @@ $(document).ready(function () {
     var price = 0;
     
     // Take Zip and Send to APIs
-    
-    $('.btn').on('click', function () {
-        var zipCode = $("#zipCode").val().trim();
-    
-    $(document).on("click", ".submit", eventInfo);
-    })
-    
-    // Call API 
         
+    $("#zipCode").on("click", eventInfo);
+        
+        // Call API 
+        
+        //     var zipCode = $("#zipCode").val().trim();
     function eventInfo() {
+        event.preventDefault();
+
+        location = $("#zip-input").val().trim();
         var year = $(this).attr("year-name");
-        var queryURL = "https://www.eventbriteapi.com/v3/users/me/?token=YMCHN5J2OTS2WEM2FF2" + year + "&api_key=dc6zaTOxFJmzC&limit=10";
+        var queryURL = "https://www.eventbriteapi.com/v3/events/search/?location.address=" + location + "&token=YMCHN5J2AOTS2WEM2FF2";
+        
     
         $.ajax({
             url: queryURL,
-            method: "GET", "relative_url":"users/me"
+            method: "GET", //"relative_url":"users/me"
         }).done(function (response) {
+            console.log(response);
     
-    // Store Results in Firebase
+    //Store Results in Firebase
     
             var results = response.data;
     
-            for (var i = 0; i < results.length; i++) {
-                var eventName = dataRef.ref().push({
-                    name: addName,
-            });
-        };
-    });
+            // for  (i = 0; i < location.length; i++) {
+            //     var eventName = dataRef.ref().push({
+            //         name: addName,
+            //     });
+            // }
+        });
+    }
     
-    $('.btn').done.on(function (results) {
-        var addName = $("#addName").val().trim();
-        var addVenue = $("#addVenue").val().trim();
-        var addStart = $("#addStart").val().trim();
-        var addEnd = $("#addEnd").val().trim();
-        var addPrice = $("#addPrice").val().trim();
+    // $('.btn').done.on(function (results) {
+    //     var addName = $("#addName").val().trim();
+    //     var addVenue = $("#addVenue").val().trim();
+    //     var addStart = $("#addStart").val().trim();
+    //     var addEnd = $("#addEnd").val().trim();
+    //     var addPrice = $("#addPrice").val().trim();
     
-        dataRef.ref().push({
-            name: addName,
-            venue: addVenue,
-            start: addStart,
-            end: addEnd,
-            price: addPrice,
-        })
+    //     dataRef.ref().push({
+    //         name: addName,
+    //         venue: addVenue,
+    //         start: addStart,
+    //         end: addEnd,
+    //         price: addPrice,
+    //     })
     
-        $("#addName").val("");
-        $("#addVenue").val("");
-        $("#addStart").val("");
-        $("#addEnd").val("");
-        $("#addPrice").val("");
+    //     $("#addName").val("");
+    //     $("#addVenue").val("");
+    //     $("#addStart").val("");
+    //     $("#addEnd").val("");
+    //     $("#addPrice").val("");
     
-        return false;
-    });
+    //     return false;
+    // });
     
-    // Link up Firebase realtime data to HTML DOM
+    // // Link up Firebase realtime data to HTML DOM
     
-    dataRef.ref().on("child_added", function (childSnapshot, prevChildKey) {
+    // dataRef.ref().on("child_added", function (childSnapshot, prevChildKey) {
     
-        var name = childSnapshot.val().name;
-        var venue = childSnapshot.val().venue;
-        var start = childSnapshot.val().start;
-        var end = childSnapshot.val().end;
-        var price = childSnapshot.val().price;
+    //     var name = childSnapshot.val().name;
+    //     var venue = childSnapshot.val().venue;
+    //     var start = childSnapshot.val().start;
+    //     var end = childSnapshot.val().end;
+    //     var price = childSnapshot.val().price;
     
-        $("#eventResults").append(
-            "<tr><td>" + name +
-            "</td><td>" + venue +
-            "</td><td>" + start +
-            "</td><td>" + end +
-            "</td><td>" + price +
-            "</td></tr>");
-    },
-    ); 
+    //     $("#eventResults").append(
+    //         "<tr><td>" + name +
+    //         "</td><td>" + venue +
+    //         "</td><td>" + start +
+    //         "</td><td>" + end +
+    //         "</td><td>" + price +
+    //         "</td></tr>");
+    // },
+    // ); 
+});
