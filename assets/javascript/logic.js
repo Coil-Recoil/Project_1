6db8d0dfc2fca408aa027e16c002445d412d9a31
Hooch <nativetongues09@gmail.com>
2018-06-28 18:27:32 -0400

    Project_1

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,13 +1,10 @@
 $(document).ready(function () {
+
     var location = "";
-    var start = 0;
 
     // Take Zip and Send to APIs
 
     $("#zipCode").on("click", eventInfo);
-   
-   /* $('.btn').on('click', function () {
-        var addName = $("#addName").val().trim();*/
 
     // Call API 
 
@@ -15,7 +12,7 @@ $(document).ready(function () {
         event.preventDefault();
 
         location = $("#zip-input").val().trim();
-        var queryURL = "https://www.eventbriteapi.com/v3/events/search/?location.address=" + location + "&token=YMCHN5J2AOTS2WEM2FF2";
+        var queryURL = "https://www.eventbriteapi.com/v3/events/search/?location.address=" + location + "&start_date.keyword=today&token=YMCHN5J2AOTS2WEM2FF2";
 
 
         $.ajax({
@@ -33,71 +30,68 @@ $(document).ready(function () {
                 var event = results[i].name.text;
                 var eventLink = results[i].url;
                 var eventName = "<a href='" + eventLink + "'>" + event + "</a>";
+                var eventTime = moment(results[i].start.local).format("LT, ddd");
+                console.log(eventTime);
                 console.log(eventLink);
 
                 $("#eventResults").append(
                     "<tr><td id='addRow'>" + eventName +
-                    "</td><td id='addRow'>" + start +
+                    "</td><td id='addRow'>" + eventTime +
                     "</td></tr>");
-
-                //var cell= $("<tr><td>" + "<a href='" + eventLink + "'>" + event + "</a></td></tr>");
             }
         });
     }
 
-$("#zipCode").on("click", weatherInfo);
+    $("#zipCode").on("click", weatherInfo);
 
     function weatherInfo() {
         event.preventDefault();
 
         location = $("#zip-input").val().trim();
-        // var APIKey = "815a2a795482a610cc6a76c55b68ac5a";
-   var queryURL = "https://api.openweathermap.org/data/2.5/forecast?zip=" + location + ",us&APPID=815a2a795482a610cc6a76c55b68ac5a";
+        var queryURL = "https://api.openweathermap.org/data/2.5/forecast?zip=" + location + ",us&APPID=815a2a795482a610cc6a76c55b68ac5a";
 
-   $.ajax({
-     url: queryURL,
-     method: "GET"
-   })
-   
-   .then(function (response) {
-     console.log(response);
-     var weatherData = response.list;
-     $("#hourlyForecast").empty();
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        })
 
-     var forecastSlotsToDisplay = 6;
-     var bounds = weatherData.length >= forecastSlotsToDisplay ? 5 : weatherData.length;
+            .then(function (response) {
+                console.log(response);
+                var weatherData = response.list;
+                $("#hourlyForecast").empty();
 
-     for (var i = 0; i <= bounds; i++) {
-       var time = moment.unix(weatherData[i].dt);
-       var currentTime = moment();
-       var diff = currentTime.diff(time)
-       if (diff > 0) {
-         continue;
-       }
-       var displayTime = time.format("LT, ddd");
-       var pullTemp = weatherData[i].main.temp;
-       var displayTemp = parseInt((pullTemp-273.15)*1.8)+32;
-       var displayWeather = weatherData[i].weather[0].description;
-       var pullIcon = weatherData[i].weather[0].icon;
-       var displayIcon= "<img src='http://openweathermap.org/img/w/" + pullIcon + ".png' id='icons'>";
-       // var weather\
+                var forecastSlotsToDisplay = 6;
+                var bounds = weatherData.length >= forecastSlotsToDisplay ? 5 : weatherData.length;
 
-       $("#weatherTime").append(
-       "<th id='addRow'>" + displayTime +
-         "</th>"
-        )
+                for (var i = 0; i <= bounds; i++) {
+                    var time = moment.unix(weatherData[i].dt);
+                    var currentTime = moment();
+                    var diff = currentTime.diff(time)
 
-        $("#weatherIcons").append(
-            "<td>" + displayIcon +
-              "</td>"
-             )
-       
-       $("#hourlyForecast").append(
-         "<td>" + displayTemp +
-         "</td>")
-     }
-   });
+                    if (diff > 0) {
+                        continue;
+                    }
+                    var displayTime = time.format("LT, ddd");
+                    var pullTemp = weatherData[i].main.temp;
+                    var displayTemp = parseInt((pullTemp - 273.15) * 1.8) + 32;
+                    //var displayWeather = weatherData[i].weather[0].description;
+                    var pullIcon = weatherData[i].weather[0].icon;
+                    var displayIcon = "<img src='http://openweathermap.org/img/w/" + pullIcon + ".png' id='icons'>";
 
-   // function displayWeather() {
-}
- });
+                    $("#weatherTime").append(
+                        "<th id='addRow'>" + displayTime +
+                        "</th>"
+                    )
+
+                    $("#weatherIcons").append(
+                        "<td>" + displayIcon +
+                        "</td>"
+                    )
+
+                    $("#hourlyForecast").append(
+                        "<td>" + displayTemp +
+                        "</td>")
+                }
+            });
+    }
+});
